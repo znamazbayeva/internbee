d45b462d98ad5af544236ea56fa32a30b4e1cd44
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2022-02-28 20:52:26 +0600

    connection to test db fixed

--- frontend/src/pages/Internships.js
+++ frontend/src/pages/Internships.js
@@ -1,49 +1,78 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import InternshipList from "../components/Internships/InternshipList";
 import SearchBar from "../components/SearchBar";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../util/Firebase";
 
 function Internships() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadedInternships, setLoadedInternships] = useState([]);
+  const [internships, setInternships] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const internshipsCollectionRef = collection(db, "internships");
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://react-app-internships-default-rtdb.firebaseio.com/internships.json"
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const internships = [];
+    const getUsers = async () => {
+      const data = await getDocs(internshipsCollectionRef);
+      setInternships(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-        for (const key in data) {
-          const internship = {
-            id: key,
-            ...data[key],
-          };
-
-          internships.push(internship);
-        }
-
-        setIsLoading(false);
-        setLoadedInternships(internships);
-      });
+    getUsers();
   }, []);
 
+  //const [loadedInternships, setLoadedInternships] = useState([]);
+
+  // const fetchInternshipsHandler = useCallback(async () => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://internbee-db-default-rtdb.firebaseio.com/internships.json"
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Somethin went wrong!");
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedInternships = [];
+
+  //     for (const key in data) {
+  //       loadedInternships.push({
+  //         id: key,
+  //         ...data[key],
+  //       });
+  //     }
+  //     setInternships(loadedInternships);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  //   setIsLoading(false);
+  // }, []);
+
+  // useEffect(() => {
+  //   fetchInternshipsHandler();
+  // }, [fetchInternshipsHandler]);
+
+  let content = <p>Found no internships.</p>;
+
+  if (internships.length > 0) {
+    content = <InternshipList internships={internships} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
   if (isLoading) {
-    return (
-      <section>
-        <p>Loading...</p>
-      </section>
-    );
+    content = <p>Loading...</p>;
   }
 
   return (
     <section>
       <SearchBar />
       <h1>All Internships</h1>
-      <InternshipList internships={loadedInternships} />
+      <section>{content}</section>
     </section>
   );
 }
--- frontend/src/pages/NewInternshipPage.js
+++ frontend/src/pages/NewInternshipPage.js
@@ -1,23 +1,30 @@
-import { useNavigate } from "react-router-dom";
+import { addDoc, collection } from "firebase/firestore";
+//import { useNavigate } from "react-router-dom";
 import NewInternshipForm from "../components/Internships/NewInternshipForm";
+import { db } from "../util/Firebase";
 
 function NewInternshipPage() {
-  const history = useNavigate();
+  //const history = useNavigate();
 
-  function addInternshipHandler(internshipData) {
-    fetch(
-      "https://react-app-internships-default-rtdb.firebaseio.com/internships.json",
-      {
-        method: "POST",
-        body: JSON.stringify(internshipData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    ).then(() => {
-      history("/");
-    });
-  }
+  // async function addInternshipHandler(internshipData) {
+  //   const response = await fetch(
+  //     "https://react-app-internships-default-rtdb.firebaseio.com/internships.json",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(internshipData),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+  const internshipsCollectionRef = collection(db, "internships");
+
+  const addInternshipHandler = async (internshipData) => {
+    await addDoc(internshipsCollectionRef, { ...internshipData });
+  };
 
   return <NewInternshipForm onAddInternship={addInternshipHandler} />;
 }
