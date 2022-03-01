c7992794a00e089eb8b146668a832901444af964
Samatqyzy <40865688+znamazbayeva@users.noreply.github.com>
2022-03-02 02:41:42 +0600

    fix: delete djoser

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -33,23 +33,3 @@ class CompanySignupSerializer(serializers.ModelSerializer):
         return user
 
 
-class StudentSignupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=User
-        fields=['email','password']
-        extra_kwargs={
-            'password':{'write_only':True}
-        }
-    
-
-    def save(self, **kwargs):
-        user=User(
-            email=self.validated_data['email']
-        )
-        password=self.validated_data['password']
-        user.set_password(password)
-        user.is_student=True
-        user.is_company=False
-        user.save()
-        Student.objects.create(user=user)
-        return user
