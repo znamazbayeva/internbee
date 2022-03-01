b08776edfc17f5c0f5c2ebc21dbbf05a535ed95d
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2022-03-02 02:11:27 +0600

    renames

--- frontend/src/components/ClientDashboard.js
+++ frontend/src/components/ClientDashboard.js
@@ -1,17 +1,17 @@
-import React, {useEffect} from 'react'
-import {getClientUser} from "../actions/auth"
-import {useDispatch} from "react-redux"
+import React, { useEffect } from "react";
+import { getStudentUser } from "../actions/auth";
+import { useDispatch } from "react-redux";
 
- function ClientDashboard() {
-     const dispatch=useDispatch()
-     useEffect(()=>{
-         dispatch(getClientUser())
-     }, [dispatch])
-    return (
-        <div className='container mt-5'>
-             welcome! we have talented developer ready for hired
-        </div>
-    )
+function ClientDashboard() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getStudentUser());
+  }, [dispatch]);
+  return (
+    <div className="container mt-5">
+      welcome! we have talented developer ready for hired
+    </div>
+  );
 }
 
 export default ClientDashboard;
