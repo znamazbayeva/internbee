8c19a7b9a68bd95d1699914f7cfd8a72c8d5eb2a
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2022-03-18 12:59:26 +0600

    Minor changes to routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import HomePage from "./components/HomePage";
 import ClientSignup from "./components/Signup/ClientSignup";
 import FreelanceSignup from "./components/Signup/FreelanceSignup";
 import Login from "./components/Login/Login";
-import ClientDashboard from "./components/ClientDashboard";
-import FreelanceDashboard from "./components/FreelanceDashboard";
+import ClientDashboard from "./components/Profile/ClientDashboard";
+import FreelanceDashboard from "./components/Profile/FreelanceDashboard";
 
 import { CPrivateRoute, FPrivateRoute } from "./private/PrivateRoute";
 
@@ -12,30 +12,30 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Navbar />
-				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route exact path="/client/signup" component={ClientSignup} />
-					<Route exact path="/freelance/signup" component={FreelanceSignup} />
-					<Route exact path="/login" component={Login} />
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/client/signup" component={ClientSignup} />
+          <Route exact path="/freelance/signup" component={FreelanceSignup} />
+          <Route exact path="/login" component={Login} />
 
-					<CPrivateRoute
-						exact
-						path="/client/dashboard"
-						component={ClientDashboard}
-					/>
-					<FPrivateRoute
-						exact
-						path="/freelance/dashboard"
-						component={FreelanceDashboard}
-					/>
-				</Switch>
-			</div>
-		</Router>
-	);
+          <CPrivateRoute
+            exact
+            path="/student/dashboard"
+            component={ClientDashboard}
+          />
+          <FPrivateRoute
+            exact
+            path="/company/dashboard"
+            component={FreelanceDashboard}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
