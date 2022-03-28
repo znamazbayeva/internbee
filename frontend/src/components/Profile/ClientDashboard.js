6e4df7138bbc69edb07b70c2914318aeb918c1b1
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2022-03-28 14:10:04 +0600

    Changed Student Profile Page

--- frontend/src/components/Profile/ClientDashboard.js
+++ frontend/src/components/Profile/ClientDashboard.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { getStudentUser } from "../../actions/auth";
 import { useDispatch, useSelector } from "react-redux";
 import StudentInfo from "./StudentInfo";
 import StudentInfo2 from "./StudenInfo2";
-import styles from "./Profile.module.scss"
-import axios from "axios"
+import styles from "./Profile.module.scss";
+import axios from "axios";
 
 function ClientDashboard() {
   const state = useSelector((state) => state.auth);
   const [student, setStudent] = useState(null);
   console.log(state.user_id);
   useEffect(() => {
-      axios
-      .get("http://127.0.0.1:8000/v1/api/student/" + (state.user_id).toString())
+    axios
+      .get("http://127.0.0.1:8000/v1/api/student/" + state.user_id.toString())
       .then((res) => {
-          setStudent(res.data);
-          console.log(student)
+        setStudent(res.data);
+        console.log(student);
       })
       .catch((err) => {
-          console.log(err)
+        console.log(err);
       });
   }, []);
   const dispatch = useDispatch();
@@ -26,9 +26,9 @@ function ClientDashboard() {
     dispatch(getStudentUser());
   }, [dispatch]);
   return (
-    <div className={styles.containter} style={{display: "flex"}}>
-        {student != null ? <StudentInfo student={student}/> : null} 
-        {student != null ? <StudentInfo2 student={student}/> : null}   
+    <div className={styles.outer_container} style={{ display: "flex" }}>
+      {student != null ? <StudentInfo student={student} /> : null}
+      {student != null ? <StudentInfo2 student={student} /> : null}
     </div>
   );
 }
--- frontend/src/components/Profile/StudentInfo.js
+++ frontend/src/components/Profile/StudentInfo.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
-import {useSelector} from "react-redux"
-import axios from "axios"
-import styles from "./Profile.module.scss"
+import { useSelector } from "react-redux";
+import axios from "axios";
+import styles from "./Profile.module.scss";
 
-function StudentInfo({student}) {
-console.log(student)
+function StudentInfo({ student }) {
+  console.log(student);
 
-return (
-    <div className={styles.student_hi}>
-            <img src={student.img} alt="" width={120} height={120}/>
-            <div>Welcome, {student.firstName} {student.lastName}!</div>
-            <div>
-                <div>Your Resume</div>
-                <div>Your ratings</div>
-                <div>Liked Internships</div>
-                <div>Applied to</div>
-            </div>
+  return (
+    <div className={styles.student_hi + " " + styles.container}>
+      <img src={student.img} alt="" width={120} height={120} />
+      <div className={styles.welcome_msg}>
+        Welcome, {student.firstName} {student.lastName}!
+      </div>
+      <div>
+        <div>My Resume</div>
+        <div>My Rating</div>
+        <div>Saved Internships</div>
+        <div>Application History</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default StudentInfo
+export default StudentInfo;
