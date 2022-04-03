45eb3e21a252527b386e099e6ed44a0c48d43a33
z.namazbayevva <40865688+znamazbayeva@users.noreply.github.com>
2022-04-03 15:49:50 +0600

    fix: the url of liking internships by student function

--- backend/internship/urls.py
+++ backend/internship/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
   path('all/', views.InternshipListView.as_view(), name='all-internships'),
   path('sorted/', views.InternshipListView.as_view(), name="sorted-internships"),
-  path('like/<int:pk>', views.InternshipLikeAPIView.as_view(),  name='internship-like'),
+  path('like/<int:pk>/', views.InternshipLikeAPIView.as_view(),  name='internship-like'),
 ]
