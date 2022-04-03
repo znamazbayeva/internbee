d2e2d8b6a37505c112c2871cf1945dbdfa412087
z.namazbayevva <40865688+znamazbayeva@users.noreply.github.com>
2022-04-03 18:39:21 +0600

    fix: minor mistakes such as unused libraries and null spaces

--- backend/internship/views.py
+++ backend/internship/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from .models import Internship, InternshipLike
 from .serializers import InternshipSerializer, InternshipLikeSerializer
